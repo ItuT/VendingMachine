28902ac7f2dec0225c37cbc44f6d928028195846
Itumeleng Tlali <tlalii663@gmail.com>
2018-02-05 05:53:46 +0200

    Products data loading

--- smart-vending-machine/src/app/app.component.ts
+++ smart-vending-machine/src/app/app.component.ts
@@ -7,18 +7,26 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'The Smartest Vending Machine The World Has Ever Seen!';
+  items:any[];
 
   constructor(private httpClient:HttpClient){
-
+    this.getProducts();
   }
 
   getProducts(){
-
+   
    this.httpClient.get('http://35.177.195.110/products?name=%22itu%22')
    .subscribe(
      (data:any) => {
-      console.log(data);
+       this.items = [];
+       let json = data["products"][0];
+       console.log(json);
+       for(var key in json){
+        console.log(json[key]);
+        this.items.push(key);
+       }
+      
      }
    )
   }
